38ef8b5e7605b44baff8fc267311612626837b68
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-06-11 17:18:58 +0200

    fetching data from components

--- src/views/jobs/JobDetails.vue
+++ src/views/jobs/JobDetails.vue
@@ -1,21 +1,24 @@
 <template>
-    <h1>Job Details Page</h1>
-    <p>The job id is {{ id }}</p>
-  <div v-for="job in jobs" :key="job.id">
-    <h2>{{ job.title }}</h2></div>
+  <h1>{{ job.title }}</h1>
+  <p>The job id is {{ id }}</p>
+  <p>{{ ijob.details }}</p>
 </template>
 
 <script>
 export default {
-    props: ['id'],
-// data() {
-//     return {
-//         id: this.$route.params.id
-//     }
-// }
-}
+  props: ["id"],
+  data() {
+    return {
+      job: null
+    }
+  },
+  mounted() {
+    fetch("http://localhost:3000/jobs" + this.id)
+      .then((res) => res.json())
+      .then((data) => (this.job = data))
+      .catch((err) => console.log(err.message));
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
